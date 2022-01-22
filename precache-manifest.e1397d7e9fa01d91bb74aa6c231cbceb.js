6678e1c7b8a24340d4e21943e7da21b10444d0ac
master <devgeorg222@gmail.com>
2022-01-22 16:15:28 +0200

    Updates

---
+++ precache-manifest.e1397d7e9fa01d91bb74aa6c231cbceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f708c4be358cb6936524107c78bd9264",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "0d4e65d015e3997d66ad",
+    "url": "/MemoryGame/static/css/main.2dfabd76.chunk.css"
+  },
+  {
+    "revision": "9894e86001343bf4f9d9",
+    "url": "/MemoryGame/static/js/2.cf040356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemoryGame/static/js/2.cf040356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4e65d015e3997d66ad",
+    "url": "/MemoryGame/static/js/main.f8020301.chunk.js"
+  },
+  {
+    "revision": "3773078a199bb6ed091f",
+    "url": "/MemoryGame/static/js/runtime-main.6e00caa5.js"
+  }
+]);
